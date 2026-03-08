55f3f8d952efcaec160cfff59a423dda58423fd2
Gilberto A. Ortiz <117542765+gaortizg@users.noreply.github.com>
2026-03-08 17:58:30 -0500

    Add files via upload
    Python If-Else challenge
    https://www.hackerrank.com/challenges/py-if-else/problem

---
+++ 02_python_if_else.py
@@ -0,0 +1,18 @@
+if __name__ == "__main__":
+    # Validate input for n (it has to be an integer and between [1,100])
+    while True:
+        try:
+            n = int(input("Enter integer between 1 and 100: ").strip())
+            if n < 1 or n > 100:
+                print("Invalid input. Please enter an integer between 1 and 100: ")
+                continue
+            break
+        except ValueError:
+            print("Invalid input. Please enter an integer between 1 and 100: ")
+            continue
+    
+    # Check conditions of challenge and print result
+    if (n % 2 == 1) or (n in range(6,21)):
+        print("Weird")
+    else:
+        print("Not Weird")
